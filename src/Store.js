f60a9e57526b64ff322514235a8e216c7dc4da5e
hatem-marzougui <hatem.adresse@gmail.com>
2020-09-09 12:52:58 +0200

    Update Store.js

--- src/Store.js
+++ src/Store.js
@@ -1,8 +1,20 @@
-import {createStore} from 'redux';
+import {createStore,applyMiddleware} from 'redux';
 import reducer from './components/Reducer';
 
 
+
+
+const logger = store => next => action => {
+    console.log('dispatching', action)
+    let result = next(action)
+    console.log('next state', store.getState())
+    return result
+   }
+
+
 export const store=createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    reducer, applyMiddleware(logger)
+   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    
+    );
 
